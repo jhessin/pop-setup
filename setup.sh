022ab63cef15f09af67283fa60af36c947690341
Jim Hessin <jhessin@gmail.com>
2021-10-12 12:22:50 -0600

    fixed kitty config

--- setup.sh
+++ setup.sh
@@ -72,12 +72,21 @@ if [ ! -d $HOMEBREW_PATH ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Install all packages in the packages directory
-sudo apt install ./packages/*.deb
+# install brew packages
+if confirm "Install brew packages?"; then
+  brew install $(cat ./brew.packages)
+fi
 
 # Install kitty terminal
 if confirm "Install kitty terminal?"; then
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+  # your PATH)
+  sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
+  # Place the kitty.desktop file somewhere it can be found by the OS
+  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+  # Update the path to the kitty icon in the kitty.desktop file
+  sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
 fi
 
 # Install nvm and node
