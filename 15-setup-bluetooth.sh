cbc75ec26879cfa9724d74a545bdfe6d4493ebe4
Jim Hessin <jhessin@gmail.com>
2021-11-13 19:41:49 -0700

    Tested and fixed bluetooth fix

--- 15-setup-bluetooth.sh
+++ 15-setup-bluetooth.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-sudo sed -i 's/load-module module-bluetooth-discover$/load-module module-bluetooth-discover headset=ofono' /etc/pulse/default.pa
+sudo sed -i 's/load-module module-bluetooth-discover$/load-module module-bluetooth-discover headset=ofono/' /etc/pulse/default.pa
 
 sudo sed '/<\/busconfig>/i <policy user="pulse">\n<allow send_destination="org.ofono"\/>\n<\/policy>' /etc/dbus-1/system.d/ofono.conf
 
-sudo echo [phonesim] >> /etc/ofono/phonesim.conf
+sudo echo \[phonesim\] >> /etc/ofono/phonesim.conf
 sudo echo Driver=phonesim >> /etc/ofono/phonesim.conf
 sudo echo Address=127.0.0.1 >> /etc/ofono/phonesim.conf
 sudo echo Port=12345 >> /etc/ofono/phonesim.conf
@@ -20,7 +20,5 @@ cd $POP_SETUP
 if confirm "Continue to ./20-setup-brew.sh"; then
   ./20-setup-brew.sh
 fi
-
-# TODO TEST THIS!
 
 # vim:foldmethod=indent
